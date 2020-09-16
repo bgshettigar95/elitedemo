6402c0ccb54fbe9b89e46d203200e607a49a86a6
Bhagyashree <bgshettigar95@gmail.com>
2020-09-16 15:06:09 +0530

    Add serverjs

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '3000';
+
+app.use(express.static(__dirname + '/dist/elite-mindz'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/elite-mindz/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at :${port}`);
+});
